720a63319f66aeffa8b27aa6a4837157877efb1c
inderpreetsingh <singhs.ishwer@gmail.com>
2014-06-01 19:38:57 +0530

    added file extension check

--- OGV/server/uploader.js
+++ OGV/server/uploader.js
@@ -1,39 +1,47 @@
 Meteor.methods({
-    saveFile: function(blob, name, path, encoding) 
+    saveFile: function(blob, name, uploadPath, encoding) 
     {
-	var path = cleanPath(path),
+	var uploadPath = cleanPath(uploadPath),
 	    fs = Npm.require('fs'),
+	    path = Npm.require('path'),
 	    name = cleanName(name || 'file'),
+	    ext = path.extname(name),
 	    encoding = encoding || 'binary',
 	    appRoot = process.env.PWD+'/private/',
 	    userId = Meteor.userId(),
-	    path = appRoot + userId + '/';
-	    
-	if (!fs.existsSync(path))
-	{
-		console.log("now creating directory");
-		fs.mkdirSync(path);
+	    uploadPath = appRoot + userId + '/';
+	
+	console.log(ext);   
+	if (!fs.existsSync(uploadPath)) {
+	    console.log("now creating directory");
+	    fs.mkdirSync(path);
 	}
-	
-	
-	fs.writeFile(path + name, blob, encoding, function(err) 
-	{
-	    if (err) {
-	        throw (new Meteor.Error(500, 'Failed to save file.', err));
-	    } else {
-		console.log('The file' + name + '(' + encoding + ') was saved to' + path);
-	    }
-	});
 
-	function cleanPath(str) {
+	if (ext == '.obj') {	
+	    fs.writeFile(uploadPath + name, blob, encoding, function(err) 
+	    {
+	        if (err) {
+	            throw (new Meteor.Error(500, 'Failed to save file.', err));
+	        } else {
+		   info = 'The file' + name + '(' + encoding + ') was saved to' + uploadPath;
+	        }
+	    });
+         } else {
+	     info = 'File type not supported';
+	 }
+
+	function cleanPath(str) 
+	{
 	    if (str) {
 		return str.replace(/\.\./g,'').replace(/\/+/g,'').replace(/^\/+/,'').replace(/\/+$/,'');
 	    }
 	}
 
-	function cleanName(str) {
+	function cleanName(str) 
+	{
 	    return str.replace(/\.\./g,'').replace(/\//g,'');
 	}
+	return info;	
     }
 });
     

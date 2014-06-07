88993f0666bb323b32708d53ba389b7e657660c0
inderpreetsingh <singhs.ishwer@gmail.com>
2014-06-07 10:34:58 +0530

    added fixtures.js

---
+++ OGV/server/fixtures.js
@@ -0,0 +1,21 @@
+if (Models.find().count() === 0) 
+{
+   Models.insert({
+	name: 'sphere.obj',
+	userid : '1',
+	lovemeter : '13'
+    });
+    
+   Models.insert({
+	name: 'cube.obj',
+	userid : '1',
+	lovemeter : '9'
+    });
+
+    
+   Models.insert({
+	name: 'diamond.obj',
+	userid : '1',
+	lovemeter : '7'
+    });
+}

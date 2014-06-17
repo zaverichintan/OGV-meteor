a0f8eece72cb232a39125f2d82ce16ccc3294642
inderpreetsingh <singhs.ishwer@gmail.com>
2014-06-17 15:34:37 +0530

    added documentation and cleaned code

--- OGV/client/main.js
+++ OGV/client/main.js
@@ -1,26 +1,64 @@
-
-/**
- * Helper for main template (main.html) that checks the variable showForgotPassword 
- * to see whether or not to show forgot password form
+/*                     M A I N . J S
+ * BRL-CAD
+ *
+ * Copyright (c) 1995-2013 United States Government as represented by
+ * the U.S. Army Research Laboratory.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this file; see the file named COPYING for more
+ * information.
  */
-  
+
+/** @file OGV/client/main.js
+ *  @brief helper file for index.html
+ *
+ *  Helper for main template (index.html) that checks for the reset 
+ *  and verify email tokens. It also takes care of the display of
+ *  forgot password form.
+ */
+
+/** 
+ * index.html helper
+ *
+ * Helper function for the template index.html that returns the
+ * session variable resetPasswordToken which is set in function
+ * @return reset password token
+ */
+   
 Template.index.helpers({
-    showForgotPassword:function()
+    resetPasswordToken: function()
     {
-	return Session.get('showForgotPassword');
+	return Session.get('resetPasswordToken');
     }
 });
 
 /**
- * set variable named resetPasswordToken, if such a token exists. Such token shall 
- * when user clicks on the reset password link in the mail it got.
+ * Sets reset password token.
+ *
+ * Checks for reset password token in the url and sets the session
+ * variable resetPasswordToken accordingly. Such token shall be 
+ * sent to user's email-id along with the link. 
  */
-
-
 if (Accounts._resetPasswordToken) {
     Session.set('resetPasswordToken', Accounts._resetPasswordToken);
 }
 
+/**
+ * Verifies email 
+ *
+ * It checks if verify email token is set or not and verifies the 
+ * email ID accordingly. Such token shall eb sent to user's email-id
+ * along with a "verify your email" link.
+ */ 
 
 if (Accounts._verifyEmailToken) {
     Accounts.verifyEmail(Accounts._verifyEmailToken, function(err) {
@@ -32,12 +70,19 @@ if (Accounts._verifyEmailToken) {
     });
 }
 
-Template.index.helpers({
-    resetPasswordToken: function()
-    {
-	return Session.get('resetPasswordToken');
-    }
-});
-
-
+/**
+ * Subscribe to collection models
+ *
+ * Subscribes to collection named models, so that it can show it
+ * in file manager and at other places. 
+ */
 Meteor.subscribe('models');
+
+/*                                                                    
+ * Local Variables:                                                   
+ * mode: JS                                                            
+ * tab-width: 8
+ * End:                                                               
+ * ex: shiftwidth=4 tabstop=8                                         
+ */
+?>

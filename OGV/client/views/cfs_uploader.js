8bb8319c056999f164808913392b1532aec4f2b0
Shubham Chauhan <shubham.chauhan1@gmail.com>
2015-07-31 17:26:41 +0530

    route errors and links

--- OGV/client/views/cfs_uploader.js
+++ OGV/client/views/cfs_uploader.js
@@ -13,7 +13,6 @@ Template.cfsUploader.events({
 
 function uploadFile(event, temp)
 {
-    
     FS.Utility.eachFile(event, function(file) {
 	var fileId;
 	var fsFile = new FS.File(file);
@@ -22,30 +21,26 @@ function uploadFile(event, temp)
 	fsFile.timeUploaded = new Date();
 	fsFile.about = "The model " + fsFile.name() + " was uploaded on " + fsFile.timeUploaded;
 	fsFile.thumbnail = new FS.File();
-	fsFile.commentsCount = 0;
 	fsFile.viewsCount = 0;
 	var currentUser = Meteor.user();
 
-	ModelFiles.insert(fsFile,function(err,fileObj) {
-	    if (err) {
-		throwError("There was some error in uploading your file, please try again/later");
-	     } else {
-		throwNotification("File Uploaded, and will appear in file manager after it's converted"); 
-		/*sAlert.error("Upload .g or .obj file only");
-	    } else {
-	    sAlert.info("File Uploaded, and will appear in file manager after it's converted", {effect: 'flip', onRouteClose: false, stack: false, timeout: 4000, position: 'top'});*/
-		Router.go("/description/" +fileObj._id);  
-	    }
-	});
-
-    Meteor.users.update(currentUser._id, {$inc:{"profile.countModels": 1}}, function(error, res) {
+	Meteor.users.update(currentUser._id, {$inc:{"profile.countModels": 1}}, function(error, res) {
 		if (error) {
 			throwError(error.reason);
 		} else {
-		throwNotification("Updated number of models for user");
+			throwNotification("Updated number of models for user");
 		}
 	});
 
-    });
+	ModelFiles.insert(fsFile,function(err) {
+	    if (err) {
+			throwError("There was some error in uploading your file, please try again/later");
+	    } else {
+			throwNotification("File Uploaded, and will appear in file manager after it's converted"); 
+		}	   
+	});
 
+	Router.go("/description/" + fsFile._id);
+
+    });
 }

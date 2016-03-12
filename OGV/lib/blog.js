ec4a1127cd96c1b6eb66078ff51fea8975b5f88f
Chintan Zaveri <zaveri_chintan@rediff.com>
2016-03-13 00:24:07 +0530

    Blog addded
    Blog addded

---
+++ OGV/lib/blog.js
@@ -0,0 +1,15 @@
+Blog.config({
+  comments: {
+    useSideComments: true, // default is false
+    allowAnonymous: true   // default is false
+  }
+});
+Blog.config({
+  rss: {
+    title: 'BRL CAD Online Geometry viewer',
+    description: 'Updates on BRLCAD Online Geometry Viewer '
+  }
+});
+Blog.config({
+  pageSize: 10
+});
